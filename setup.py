fab8849c7801d414706053bc5b96574c4cdf1499
Okami <okami@fuzetsu.info>
2014-12-03 14:15:35 +0300

    missing static added

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import os, sys, glob, fnmatch
 from setuptools import setup
 
 
@@ -9,9 +9,48 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+# Code borrowed from wxPython's setup and config files
+# Thanks to Robin Dunn for the suggestion.
+# I am not 100% sure what's going on, but it works!
+def opj(*args):
+    path = os.path.join(*args)
+    return os.path.normpath(path)
+
+
+# https://wiki.python.org/moin/Distutils/Tutorial
+def find_data_files(srcdir, *wildcards, **kw):
+    # get a list of all files under the srcdir matching wildcards,
+    # returned in a format to be used for install_data
+    def walk_helper(arg, dirname, files):
+        if '.svn' in dirname:
+            return
+        names = []
+        lst, wildcards = arg
+        for wc in wildcards:
+            wc_name = opj(dirname, wc)
+            for f in files:
+                filename = opj(dirname, f)
+
+                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    names.append(filename)
+        if names:
+            lst.append((dirname, names))
+
+    file_list = []
+    recursive = kw.get('recursive', True)
+    if recursive:
+        os.walk(srcdir, walk_helper, (file_list, wildcards))
+    else:
+        walk_helper((file_list, wildcards),
+                    srcdir,
+                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    return file_list
+
+
 data = {
     'name': 'django-elfinderfs',
-    'version': '0.0.3',
+    'version': '0.0.4',
     'author': 'Okami',
     'author_email': 'okami@fuzetsu.info',
     'description': (
@@ -20,11 +59,15 @@ data = {
         "does not uses any databases."),
     'license': 'GPLv3',
     'keywords': 'django elfinder file manager',
-    'url': 'http://packages.python.org/django-elfinderfs',
-    'packages': ['elfinderfs'],
+    'url': 'https://github.com/okami-1/django-elfinderfs',
+    'packages': [
+        'elfinderfs',
+        'elfinderfs.migrations',
+    ],
     'long_description': '',
     'classifiers': [
         'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Framework :: Django',
     ],
@@ -33,6 +76,8 @@ data = {
         'Pillow >= 2.2.1',
         'djangorestframework >= 2.4.3',
     ],
+    'data_files': find_data_files('elfinderfs/static', '*.*'),
+    'include_package_data': True,
 }
 
 setup(**data)
